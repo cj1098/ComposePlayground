d1ebbaa4edcbfed5da11eb084ce301b879a843ed
Chris <cj1098silver@gmail.com>
2021-08-25 01:26:39 -0700

    Split layouts done!

--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composeplayground.layouts.Direction
+import com.example.composeplayground.layouts.SplitColumn
 import com.example.composeplayground.layouts.SplitRow
+import com.example.composeplayground.layouts.SplitRowLayoutComposable
 import com.example.composeplayground.ui.theme.ComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,25 +23,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePlaygroundTheme {
-                SplitRow(modifier = Modifier.fillMaxSize(),composables = listOf({ Text(text = "testText") }, { RadioButton(
-                    selected = true,
-                    onClick = { /*TODO*/ }) }))
+                SplitRow(modifier = Modifier.fillMaxWidth(), composables = listOf(
+                    SplitRowLayoutComposable(direction = Direction.START) { Text(text = "testText") },
+                    SplitRowLayoutComposable(direction = Direction.END) { RadioButton(selected = true, onClick = {  }) }))
             }
         }
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SplitRowPreview() {
+    SplitRow(modifier = Modifier.fillMaxWidth(), composables = listOf(
+        SplitRowLayoutComposable(direction = Direction.START) { Text(text = "testText") },
+        SplitRowLayoutComposable(direction = Direction.END) { RadioButton(selected = true, onClick = {  }) },
+        SplitRowLayoutComposable(direction = Direction.END) { Text(text = "oooooh") },
+        SplitRowLayoutComposable(direction = Direction.START) { Text(modifier = Modifier.offset(x = 16.dp) ,text = "oooooh") }))
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposePlaygroundTheme {
-        SplitRow(modifier = Modifier.fillMaxSize(), composables = listOf({ Text(text = "testText") }, { Button(
-            onClick = {  }, content = {
-                Text(text = "test") }) }))
-    }
+fun SplitColumnPreview() {
+    SplitColumn(modifier = Modifier.fillMaxHeight(), composables = listOf(
+        SplitRowLayoutComposable(direction = Direction.START) { Text(text = "testText") },
+        SplitRowLayoutComposable(direction = Direction.END) { RadioButton(selected = true, onClick = {  }) },
+        SplitRowLayoutComposable(direction = Direction.END) { Text(text = "oooooh") },
+        SplitRowLayoutComposable(direction = Direction.START) { Text(modifier = Modifier.offset(x = 16.dp) ,text = "oooooh") }))
 }
--- app/src/main/java/com/example/composeplayground/layouts/SplitLayouts.kt
+++ app/src/main/java/com/example/composeplayground/layouts/SplitLayouts.kt
@@ -6,21 +6,72 @@ import androidx.compose.ui.layout.Layout
 
 enum class Direction {
     START,
+    MIDDLE, // TODO?
     END
 }
 
+data class SplitRowLayoutComposable(val direction: Direction, val composable: @Composable () -> Unit)
+
 @Composable
-fun SplitRow(modifier: Modifier, composables: List<@Composable () -> Unit>) {
+fun SplitRow(modifier: Modifier, composables: List<SplitRowLayoutComposable>) {
     Layout(modifier = modifier, content = { composables.forEach {
-        it()
+        it.composable()
     } }, measurePolicy = { measurables, constraints ->
-        val placeables = measurables.map {
-            val placeable = it.measure(constraints)
-            placeable
+        val placeables = measurables.mapIndexed { index, measureable ->
+            Pair(measureable.measure(constraints.copy(minWidth = 0, minHeight = 0)), composables[index].direction)
         }
+        // TODO: If all placeables can't fit on screen (too wide) then log it.
         layout(constraints.maxWidth, constraints.maxHeight) {
-            placeables[0].placeRelative(0, 0)
-            placeables[1].placeRelative(constraints.maxWidth - placeables[1].width, 0)
+            var startX = 0
+            var endX = constraints.maxWidth
+            placeables.forEach {
+                when (it.second) {
+                    Direction.START -> {
+                        if (startX + it.first.width < endX) {
+                            it.first.placeRelative(startX, 0)
+                            startX += it.first.width
+                        }
+                    }
+                    Direction.END -> {
+                        if (endX - it.first.width > startX) {
+                            endX -= it.first.width
+                            it.first.placeRelative(endX, 0)
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+@Composable
+fun SplitColumn(modifier: Modifier, composables: List<SplitRowLayoutComposable>) {
+    Layout(modifier = modifier, content = { composables.forEach {
+        it.composable()
+    } }, measurePolicy = { measurables, constraints ->
+        val placeables = measurables.mapIndexed { index, measureable ->
+            Pair(measureable.measure(constraints.copy(minWidth = 0, minHeight = 0)), composables[index].direction)
+        }
+        // TODO: If all placeables can't fit on screen (too wide) then log it.
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            var startY = 0
+            var endY = constraints.maxHeight
+            placeables.forEach {
+                when (it.second) {
+                    Direction.START -> {
+                        if (startY + it.first.height < endY) {
+                            it.first.placeRelative(0, startY)
+                            startY += it.first.height
+                        }
+                    }
+                    Direction.END -> {
+                        if (endY - it.first.height > startY) {
+                            endY -= it.first.height
+                            it.first.placeRelative(0, endY)
+                        }
+                    }
+                }
+            }
         }
     })
 }
